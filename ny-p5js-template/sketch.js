4f757f1db7efbb63408d6ed376bf9bcd5a621b2d
Newyellow <newyellow2@gmail.com>
2023-10-20 11:59:58 +0800

    update the folder structure for faster copy paste project

--- ny-p5js-template/sketch.js
+++ ny-p5js-template/sketch.js
@@ -82,33 +82,6 @@ async function setup() {
 
     rect(200 + i, 500, 1, 60);
   }
-
-
-  // let L1 = 85.49;
-  // let C1 = 81.86;
-  // let H1 = 88.48;
-
-  // let L2 = 29.97;
-  // let C2 = 43.85;
-  // let H2 = 282.66;
-
-
-  // colorMode(RGB);
-
-  // for (let i = 0; i < 400; i++) {
-  //   let t = i / 400;
-
-  //   let nowL = 80;
-  //   let nowC = 100;
-  //   let nowH = lerp(0, 120, t);
-
-  //   let rgbValues = lchToRgb(nowL, nowC, nowH);
-
-  //   fill(rgbValues.r, rgbValues.g, rgbValues.b);
-  //   noStroke();
-
-  //   rect(200 + i, 400, 1, 60);
-  // }
 }
 
 // async sleep
